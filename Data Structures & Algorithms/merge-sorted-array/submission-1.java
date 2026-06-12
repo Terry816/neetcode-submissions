b36f6f0c339c62c2297a117f7da1f8200a04ea0c
Terry B Kim <97275921+Terry816@users.noreply.github.com>
2026-06-12 18:23:01 -0400

    Add: merge-sorted-array - submission-1

---
+++ Data Structures & Algorithms/merge-sorted-array/submission-1.java
@@ -0,0 +1,33 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        /*
+        Pointer on ends of both arrays.
+        Compare and store the greater value.
+        if nums1 empties first -> fill in rest of values till nums2 runs out
+        if nums2 empties first -> dont do anything just return
+        [1, 2, 3, 0, 0, 0]
+        [2, 5, 6]
+        */
+        if (n==0) { return; }
+
+        int i = m-1, j = n-1;
+        int p = n + m - 1;
+        while (i >= 0 && j >= 0){
+            int n1 = nums1[i], n2 = nums2[j];
+            if (n2 >= n1){
+                nums1[p] = n2;
+                j--;
+            } else {
+                nums1[p] = n1;
+                i--;
+            }
+            p--;
+        }
+        while (j >= 0){
+            nums1[p] = nums2[j];
+            j--;
+            p--;
+        }
+        
+    }
+}
